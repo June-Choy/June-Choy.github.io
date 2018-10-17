e448dc3bd8a971f157b136bf5461ec9fe05f89c2
june <june.choy@uwcsea.edu.sg>
2018-10-18 00:09:16 +0800

    final changed for 99 bottles for Day 3

--- 99bottles.js
+++ 99bottles.js
@@ -1,15 +1,4 @@
 
-//var count= "99"
-//var line1= "bottles of beer on the wall,"  
-//var line2 = count1 + " " + "bottles of beer."
-//var line3= "Take one down and pass it around,"
-//var count4= parseInt(count)
-//var line4 = "bottles of beer on the wall.";
-
-//var verse= (count1 + " " + line1 + " " + line2 + " " + line3 + " " + count4 + " " + line4 + " ")
-//verse.repeat(3)
-//console.log((count1 + " " + line1 + " " + line2 + " " + line3 + " " + count4 + " " + line4 + " "))
-
 var line1= "bottles of beer on the wall,";
 var line2 = "bottles of beer.";
 
@@ -17,15 +6,24 @@ var num = 100;
 
 var line3 = "Take one down and pass it around,";
 var line4 = "bottles of beer on the wall.";
+var line5 = "no more bottles of beer on the wall."
+var line6= "bottle of beer on the wall,";
+var line7 = "bottle of beer.";
+
 
 var num2 = 99;
 
 
-while (num > 3) {
+while (num >1) {
     num -= 1; //count = count - 1; 
-    //var result = userInput * count;
+    num2 -= 1; 
+    if (num != 1)
     console.log(num + " " + line1 + " " + num + " " + line2 + " " + line3 + " " + num2 + " " + line4)
-}
+} if (num == 1 ) {
+    console.log(num + " " + line6 + " " + num + " " + line7 + " " + line3 + " " + line5)
+} 
+    console.log("No more bottles of beer on the wall," + " " + "no more bottles of beer." + " " + "Go to the store and buy some more," + " " + "99 bottles of beer on the wall.")
+
 
 
 
